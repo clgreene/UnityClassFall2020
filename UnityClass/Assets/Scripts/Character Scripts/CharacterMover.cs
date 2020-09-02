26192cb62003afdb1abb689d8ef0c8d2e3a039c9
Carter <clgaspect@gmail.com>
2020-09-02 12:53:53 -0600

    move and fall

---
+++ UnityClass/Assets/Scripts/Character Scripts/CharacterMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Vector3 movement;
+    public float jumpForce = 25;
+    public float moveSpeed = 1;
+    public float gravity = -7.0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal") * moveSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            movement.y = jumpForce;
+        }
+
+        if (controller.isGrounded)
+        {
+            movement.y = 0;
+        }
+
+        else movement.y = gravity;
+
+        controller.Move(movement * Time.deltaTime);        
+
+    }
+}
