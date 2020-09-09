bc6c5cbc34ef51ea5005508891674f44b1616cfd
Carter <clgaspect@gmail.com>
2020-09-09 14:15:59 -0600

    Homework for spawn point, health and dmg, and death.

--- UnityClass/Assets/Scripts/Character Scripts/CharacterMover.cs
+++ UnityClass/Assets/Scripts/Character Scripts/CharacterMover.cs
@@ -10,11 +10,15 @@ public class CharacterMover : MonoBehaviour
     private CharacterController controller;
     private Vector3 movement;
     public int jumpCountMax = 2;
-    private int jumpCount;
+    public int jumpCount = 0;
     public float gravity = -7.0f, rotateSpeed = 100f, jumpForce = 500;
     private float yVar;
 
     public FloatData moveSpeed, normalSpeed, sprintSpeed;
+
+    public IntData playerJumpCount;
+
+    public Vector3Data currentSpawnPoint;
 
     private void Start()
     {
@@ -53,12 +57,22 @@ public class CharacterMover : MonoBehaviour
             jumpCount = 0;
         }
 
-        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        if (Input.GetButtonDown("Jump") && jumpCount < playerJumpCount.value)
         {
             yVar = jumpForce;
             jumpCount++;
         }
 
         controller.Move(movement * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //set location data of player to the current Spawn Point
+    }
+
+    private void OnEnable()
+    {
+        //set position of player to the location data of the player
     }
 }
---
+++ UnityClass/Assets/Scripts/jumpPowerUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class jumpPowerUp : MonoBehaviour
+{
+    public IntData playerJumpCount, powerUpCount, normalJumpCount;
+    public float waitTime = 4f;
+
+    private void Start()
+    {
+        playerJumpCount.value = normalJumpCount.value;
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    playerJumpCount.value = powerUpCount.value;
+    //}
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        playerJumpCount.value = powerUpCount.value;
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        yield return new WaitForSeconds(waitTime);
+        playerJumpCount.value = normalJumpCount.value;
+        gameObject.SetActive(false);
+    }
+
+}
