080ca90c1176211c3006b6aecc91afee00b7a478
Carter <clgaspect@gmail.com>
2020-09-19 12:34:53 -0600

    Starting on the Turn Based Combat System

---
+++ Hellish Concept/Assets/BattleSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    unit playerUnit;
+    unit enemyUnit;
+
+    public Text dialogueText;
+
+    public BattleState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<unit>();
+
+
+        dialogueText.text = "A wild " + enemyUnit.unitName + "appears!";
+    }
+
+}
---
+++ Hellish Concept/Assets/unit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unit : MonoBehaviour
+{
+
+    public string unitName;
+    public int unitLvl;
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+
+
+}
