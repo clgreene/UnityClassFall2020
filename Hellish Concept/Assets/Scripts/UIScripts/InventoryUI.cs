9309476063f188fb53174f85514d8712e643fb7c
Carter <clgaspect@gmail.com>
2020-09-27 15:38:31 -0600

    inventory System on/off switch and aesthetic

---
+++ Hellish Concept/Assets/Scripts/UIScripts/InventoryUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject inventoryUI;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && inventoryUI.activeSelf == true)
+        {
+            Debug.Log("KeyPressed");
+            inventoryUI.SetActive(false);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.F) && inventoryUI.activeSelf == false)
+        {
+            Debug.Log("KeyPressed");
+            inventoryUI.SetActive(true);
+        }
+    }
+}
