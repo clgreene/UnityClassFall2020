2164d9ddcfc4684c6af07348d71a0e10701dbf5c
Carter <clgaspect@gmail.com>
2020-10-03 12:11:11 -0600

    Creating the battle scene for Hellish Game

--- Hellish Concept/Assets/Scripts/Battle Scripts/BattleSystem.cs
+++ Hellish Concept/Assets/Scripts/Battle Scripts/BattleSystem.cs
@@ -7,12 +7,14 @@ public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
 public class BattleSystem : MonoBehaviour
 {
-
+    //calling the hellspawn object and the enemy hellspawn object
     public GameObject playerPrefab;
     public GameObject enemyPrefab;
 
+    //calling the positions the hellspawn and enemy should go
     public Transform playerBattleStation;
     public Transform enemyBattleStation;
+
 
     unit playerUnit;
     unit enemyUnit;
@@ -30,14 +32,16 @@ public class BattleSystem : MonoBehaviour
 
     void SetupBattle()
     {
+        //placing hellspawn prefab on hellspawn location
         GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
         playerUnit = playerGO.GetComponent<unit>();
 
+        //placing enemy prefab on enemy location
         GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
         enemyUnit = enemyGO.GetComponent<unit>();
 
 
-        dialogueText.text = "A wild " + enemyUnit.unitName + "appears!";
+        dialogueText.text = "A " + enemyUnit.unitName + "attacks!";
     }
 
 }
