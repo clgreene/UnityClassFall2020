5b5fd515bb1ec15e26ff21df41055d984f218a9d
Carter <clgaspect@gmail.com>
2020-10-13 19:27:14 -0600

    Setting up new Unit scripts and attacks Move scripts

---
+++ Hellish Concept/Assets/Scripts/Battle Scripts/Move.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Move : ScriptableObject
+{
+
+    public string moveName;
+    public int moveDamage;
+}
--- Hellish Concept/Assets/Scripts/Battle Scripts/Unit.cs
+++ Hellish Concept/Assets/Scripts/Battle Scripts/Unit.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour
+[CreateAssetMenu]
+
+public class Unit : ScriptableObject
 {
 
     public string unitName;
     public int unitLevel;
+    public int xp;
+    public Sprite unitSprite;
+    public int unitType;
 
+    public int baseDamage;
     public int damage;
     public int armor;
-    public int hp;
+    public int baseHP;
+    public int HP;
+    public int baseSpeed;
     public int speed;
 
-    public int maxHP;
     public int currentHP;
 
+    public Move moveOne;
+
+    public void typeOneMonster()
+    {
+        
+    }
+
+    public void typeTwoMonster()
+    {
+
+    }
 
 }
