5ac9267d6ee68da3a8fd6c68253c2a6d3623180f
Carter <clgaspect@gmail.com>
2020-11-03 21:30:27 -0700

    More level work

--- Hellish Concept/Assets/Scripts/CamDrift.cs
+++ Hellish Concept/Assets/Scripts/CamDrift.cs
@@ -6,10 +6,11 @@ public class CamDrift : MonoBehaviour
 {
 
     public float movement;
+    public int drag;
     void Update()
     {
         
-        transform.Translate(0, Mathf.Cos(Time.time) * movement, 0);
+        transform.Translate(0, Mathf.Cos(Time.time - drag) * movement, 0);
         //transform.Translate(0, Mathf.Pow(-1, Time.time) * movement, 0);
     }
 }
