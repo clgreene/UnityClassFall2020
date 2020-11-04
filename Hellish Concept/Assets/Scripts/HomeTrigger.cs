90fcca9570c823db8746840da9f45dde6d5b0409
Carter <clgaspect@gmail.com>
2020-11-04 12:42:37 -0700

    More work on level

---
+++ Hellish Concept/Assets/Scripts/HomeTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeTrigger : MonoBehaviour
+{
+
+    public GameObject obj;
+    public Color oldColor;
+    public Color newColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            oldColor = obj.GetComponent<Renderer>().material.color;
+            newColor = new Color(oldColor.r, oldColor.g, oldColor.b, oldColor.a - 0.8f);
+            obj.GetComponent<Renderer>().material.color = newColor;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            obj.GetComponent<Renderer>().material.color = oldColor;
+        }
+    }
+
+}
