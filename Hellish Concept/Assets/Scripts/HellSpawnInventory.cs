91c7ff87118f89bcc18d083e1234c37f006e9244
Carter <clgaspect@gmail.com>
2020-11-11 12:02:03 -0700

    Hellspawn pickup triggers working

---
+++ Hellish Concept/Assets/Scripts/HellSpawnInventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellSpawnInventory : MonoBehaviour
+{
+    public bool starter;
+
+    public bool Hellhound;
+    public bool Ghoul;
+    public bool Imp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Hellish Concept/Assets/Scripts/HellSpawnPickkup.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HellSpawnPickkup : MonoBehaviour
+{
+
+    
+    public Text text;
+    public string info;
+    public BoolData hellspawn;
+    public BoolData hellspawnSecond;
+    public BoolData hellspawnThird;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            text.text = info;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.F))
+        {
+            text.text = info;
+            if (hellspawnSecond.value == false && hellspawnThird.value == false) 
+            {
+                hellspawn.value = true;
+                text.text = "You obtained a Hellspawn.";
+            }
+
+            else text.text = "You already chose one, don't be greedy!";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            text.text = "";
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
