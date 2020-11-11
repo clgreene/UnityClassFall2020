5b68160b35255ef112231150c5874afb44c6acc6
Carter <clgaspect@gmail.com>
2020-11-11 14:48:56 -0700

    trigger for box searching

---
+++ Hellish Concept/Assets/Scripts/SearchTrigger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SearchTrigger : MonoBehaviour
+{
+
+    public Text text;
+    public bool shov;
+    public BoolData shovel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            text.text = "Search";
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.F))
+        {
+            if (shov == true && shovel.value == false)
+            {
+                shovel.value = true;
+                text.text = "You found a shovel";
+            }
+
+            else text.text = "You didn't find anything";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            text.text = "";
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
